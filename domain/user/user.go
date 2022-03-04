493fd7747b4b9cf66241060c8880f65609d9d803
Rekseto <ReksetoAjxer@gmail.com>
2022-03-04 01:38:53 +0100

    Social type

--- domain/user/user.go
+++ domain/user/user.go
@@ -1,3 +1,21 @@
 package user
 
-type User struct {}
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"time"
+)
+
+type User struct{}
+
+type Social struct {
+	UserID       uuid.UUID       `json:"user_id"`
+	Social       string          `json:"social"`
+	SocialId     string          `json:"social_id"`
+	AccessToken  string          `json:"access_token"`
+	RefreshToken string          `json:"refresh_token"`
+	UserData     json.RawMessage `json:"user_data"`
+	CreatedAt    time.Time       `json:"created_at"`
+	UpdatedAt    time.Time       `json:"updated_at"`
+	DestroyedAt  time.Time       `json:"destroyed_at"`
+}
