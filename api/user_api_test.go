fb60156ee7d003caa3da71c4f9f0be34126909f8
Maciej Rosiak <maciejrosiak@protonmail.com>
2022-03-30 12:05:27 +0200

    Removed old commented code

--- api/user_api_test.go
+++ api/user_api_test.go
@@ -20,49 +20,6 @@ import (
 	"time"
 )
 
-// var host = "127.0.0.1:8001"
-//func createUserAPI() http.Client {
-//	logger := log.NewEntry(log.New())
-//
-//	cfg, err := common.NewConfigFromFile("../config/configuration.test.local.json")
-//	if err != nil {
-//		log.WithError(err).Fatal("failed initializing config")
-//	}
-//
-//	db, err := common.InitPostgres(&cfg.Postgres)
-//	if err != nil {
-//		log.WithError(err).Fatal("failed initializing postgres")
-//	}
-//
-//	userRepo := user.NewPostgresUserRepository(db)
-//
-//	app := application.Application{
-//		Commands: application.Commands{
-//			AddUser: board.NewAddUser(userRepo),
-//		},
-//		Queries:  application.Queries{
-//			UserExists: board.NewUserExists(userRepo),
-//		},
-//	}
-//
-//	router := mux.NewRouter()
-//	router.Use(BodyLimitMiddleware)
-//	router.Use(LoggingMiddleware(logger))
-//	NewUserAPI(router, logger, app)
-//
-//	srv := &http.Server{
-//		Handler: router,
-//		Addr:    host,
-//		WriteTimeout: 4 * time.Second,
-//		ReadTimeout:  4 * time.Second,
-//	}
-//
-//	go log.Fatal(srv.ListenAndServe())
-//	return http.Client{
-//		Timeout: time.Second*3,
-//	}
-//}
-
 func newStringPtr(s string) *string {
 	return &s
 }
